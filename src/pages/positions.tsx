c7b017c3745507de3c592219edbc7a3e4fc0a277
msawada <msawada@folio-sec.com>
2021-04-17 15:58:53 +0900

    create link to positions page with next link

--- src/pages/positions.tsx
+++ src/pages/positions.tsx
@@ -1,22 +1,18 @@
 import Head from "next/head"
 import Header from "../components/header"
 import styles from "../styles/Index.module.css"
-import Link from "next/link"
 
-export default function Home() {
+export default function Positions() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Slow Grow - Positions</title>
+        <title>Positions</title>
         <link rel="icon" href={"/slow.png"} />
       </Head>
 
       <Header />
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to Top Page!
-        </h1>
-        <Link href={`/positions`}> positions </Link>
+        <h1 className={styles.title}> Welcome to Positions! </h1>
       </main>
     </div>
   )
